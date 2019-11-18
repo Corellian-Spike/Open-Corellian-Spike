e375b9b3e016369cd19bbce787e271bb8dc57e48
Ender Smith <Ender Smith>
2019-11-17 23:20:21 -0800

    example-game

---
+++ example-game.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const Game = require('./lib/game');
+
+const game = new Game(2);
+
+console.log('\nSTART GAME');
+game.start();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+game.table.deck.shuffle();
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+game.table.players[0].ante();
+console.log('\tpot:', game.table.pot, '| sabacc pot:', game.table.sabaccPot);
+console.log('\tplayer1 remaining credits:', game.table.players[0].credits);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+game.table.players[1].ante();
+console.log('\tpot:', game.table.pot, '| sabacc pot:', game.table.sabaccPot);
+console.log('\tplayer2 remaining credits:', game.table.players[0].credits);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+game.table.deal();
+console.log('\tdiscard top:', game.table.discard.top.value);
+console.log('\tplayer 1 hand:', game.table.players[0].hand[0].value, game.table.players[0].hand[1].value, '| sum:', game.table.players[0].sum);
+console.log('\tplayer 2 hand:', game.table.players[1].hand[0].value, game.table.players[1].hand[1].value, '| sum:', game.table.players[1].sum);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+game.table.players[0].buyFromDeck();
+console.log('   PLAYER 1 BUYS CARD FROM DECK');
+console.log('\tpot:', game.table.pot, '| sabacc pot:', game.table.sabaccPot);
+console.log('\tplayer1 remaining credits:', game.table.players[0].credits);
+console.log('\tplayer 1 hand:', game.table.players[0].hand[0].value, game.table.players[0].hand[1].value, game.table.players[0].hand[2].value, '| sum:', game.table.players[0].sum);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+game.table.players[1].swapFromDeckByCardIndex(1);
+console.log('   PLAYER 2 SWAPS CARD FROM DECK');
+console.log('\tdiscard top:', game.table.discard.top.value);
+console.log('\tplayer 2 hand:', game.table.players[1].hand[0].value, game.table.players[1].hand[1].value, '| sum:', game.table.players[1].sum);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+let spike = game.table.rollSpikeDice();
+console.log('   SPIKE ROLL:', spike.rollOne, spike.rollTwo);
+if (spike.spikeDiceMatch) {
+  console.log('\tdice match, new hands are drawn:');
+  game.table.spikeHandSwap();
+} else {
+  console.log('\tdice do not match, players keep their hands:');
+}
+console.log('\tplayer 1 hand:', game.table.players[0].hand[0].value, game.table.players[0].hand[1].value, game.table.players[0].hand[2].value, '| sum:', game.table.players[0].sum);
+console.log('\tplayer 2 hand:', game.table.players[1].hand[0].value, game.table.players[1].hand[1].value, '| sum:', game.table.players[1].sum);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+console.log('   (betting not yet implemented)');
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+console.log('   (betting not yet implemented)');
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+game.table.players[0].swapFromDiscardByCardIndex(0);
+console.log('   PLAYER 1 SWAPS CARD FROM DISCARD');
+console.log('\tdiscard top:', game.table.discard.top.value);
+console.log('\tplayer 1 hand:', game.table.players[0].hand[0].value, game.table.players[0].hand[1].value, game.table.players[0].hand[2].value, '| sum:', game.table.players[0].sum);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+game.table.players[1].buyFromDiscard();
+console.log('   PLAYER 2 BUYS TOP CARD FROM DISCARD');
+console.log('\tdiscard top:', game.table.discard.top.value);
+console.log('\tplayer 2 hand:', game.table.players[1].hand[0].value, game.table.players[1].hand[1].value, game.table.players[1].hand[2].value, '| sum:', game.table.players[1].sum);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+if (spike.spikeDiceMatch) {
+  console.log('\tdice match, new hands are drawn:');
+  game.table.spikeHandSwap();
+} else {
+  console.log('\tdice do not match, players keep their hands:');
+}
+console.log('\tplayer 1 hand:', game.table.players[0].hand[0].value, game.table.players[0].hand[1].value, game.table.players[0].hand[2].value, '| sum:', game.table.players[0].sum);
+console.log('\tplayer 2 hand:', game.table.players[1].hand[0].value, game.table.players[1].hand[1].value, '| sum:', game.table.players[1].sum);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+console.log('   (betting not yet implemented)');
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+console.log('   (betting not yet implemented)');
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+console.log('   PLAYER 1 STANDS');
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+game.table.players[1].buyFromDeck();
+console.log('   PLAYER 2 BUYS CARD FROM DECK');
+console.log('\tpot:', game.table.pot, '| sabacc pot:', game.table.sabaccPot);
+console.log('\tplayer2 remaining credits:', game.table.players[1].credits);
+console.log('\tplayer 2 hand:', game.table.players[1].hand[0].value, game.table.players[1].hand[1].value, game.table.players[1].hand[2].value, game.table.players[1].hand[3].value, '| sum:', game.table.players[1].sum);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+if (spike.spikeDiceMatch) {
+  console.log('\tdice match, new hands are drawn:');
+  game.table.spikeHandSwap();
+} else {
+  console.log('\tdice do not match, players keep their hands:');
+}
+console.log('\tplayer 1 hand:', game.table.players[0].hand[0].value, game.table.players[0].hand[1].value, game.table.players[0].hand[2].value, '| sum:', game.table.players[0].sum);
+console.log('\tplayer 2 hand:', game.table.players[1].hand[0].value, game.table.players[1].hand[1].value, '| sum:', game.table.players[1].sum);
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+console.log('   (betting not yet implemented)');
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
+console.log('   (betting not yet implemented)');
+game.loopStep();
+console.log('\n', 'round:', game.round, '| phase:', game.phase, '| turn:', game.turn);
--- index.js
+++ index.js
@@ -6,18 +6,36 @@ const io = require('socket.io')(http);
 
 const Game = require('./lib/game');
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+let sequentialId = 0;
+let game = new Game(2);
+
+io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.emit('welcome')
-  socket.on('disconnect', function(){
+  socket.emit('check-id');
+  socket.on('no-id', () => {
+    socket.emit('new-id', sequentialId);
+    console.log('issuing id:', sequentialId++)
+  });
+  socket.on('show-id', (id) => {
+    console.log('showed id:', id);
+    if (id === 1) {
+      game.start();
+      io.emit('gameInfoPublic', game.infoPublic);
+      console.log('starting game');
+    };
+  });
+  socket.on('getPlayer', (id) => {
+    socket.emit('sendPlayer', game.table.players[id].infoPrivate)
+  });
+  socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
-http.listen(3000, function(){
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
