207ebdbda5fd12ba2de783e4f5f948018074d874
Ender Smith <Ender Smith>
2019-11-20 23:47:11 -0800

    continuing to refactor

--- example-game.js
+++ example-game.js
@@ -2,8 +2,6 @@
 
 const Game = require('./lib/game');
 
-const game = new Game(8);
-game.deck.shuffle();
-game.deck.dealTo(game.players);
-game.deck.discardTo(game.discard)
-console.log(game.scoreActivePlayers());
+const game = new Game(3);
+game.start();
+game.continue();
